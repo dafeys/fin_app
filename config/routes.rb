433be83dfa842db2ee21bdad06228848e8e69574
dafeys <dafeys@outlook.com>
2023-06-27 23:27:49 +0300

    Add basic reporst controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'reports/index'
+  get 'reports/report_by_category'
+  get 'reports/report_by_dates'
   get 'main/index'
   resources :operations
   resources :categories
+  resources :reports
   root "main#index"
 end
