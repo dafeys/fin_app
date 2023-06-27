b2615985ebdcb632d5ee08e9b0f580a671f0eb82
dafeys <dafeys@outlook.com>
2023-06-27 22:35:00 +0300

    add validation to category and operation

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,5 @@
 class Operation < ApplicationRecord
   belongs_to :category
+  validates :amount, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :odate, :description, presence: true
 end
